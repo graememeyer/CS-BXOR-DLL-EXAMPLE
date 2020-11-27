0d86d94777cc7dfefae74e60564a2c31d6bdd48f
Graeme Meyer <graememeyer@hotmail.com>
2020-11-27 09:30:26 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_BXOR_DLL_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
